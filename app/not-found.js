d9e0e2065260fbda8c96f827072afe3b6326cdef
DussanFreire <dussanfreire@gmail.com>
2024-09-24 20:39:30 -0400

    feat: supence added

---
+++ app/not-found.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+function NotFound() {
+  return (
+    <main className="mt-4 space-y-6 text-center">
+      <h1 className="text-3xl font-semibold">
+        This page could not be found :(
+      </h1>
+      <Link
+        href="/"
+        className="inline-block bg-accent-500 px-6 py-3 text-lg text-primary-800"
+      >
+        Go back home
+      </Link>
+    </main>
+  );
+}
+
+export default NotFound;
